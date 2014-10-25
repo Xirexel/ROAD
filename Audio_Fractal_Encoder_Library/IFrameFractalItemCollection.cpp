679d6681265d4444f95dbeeb24af1158df08470d
Xirexel <Xirexel@users.noreply.github.com>
2014-10-25 16:51:53 +1100

    Clear branch

--- Audio_Fractal_Encoder_Library/IFrameFractalItemCollection.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-#include <exception>
-using namespace std;
-
-#include "IFrameFractalItemCollection.h"
-#include "IFractalItem.h"
-
-
--- Audio_Fractal_Encoder_Library/IRawDataWriter.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-#include <exception>
-using namespace std;
-
-#include "IRawDataWriter.h"
-#include "IFractalDataContainer.h"
-
-
